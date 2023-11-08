a6b5854be74ee4b57143e83e65638fb96a95b705
emiz6413 <mizutanieisuke+github.com>
2023-11-08 17:19:36 +0800

    apply spectral norm only to discriminator

--- src/biggan.py
+++ src/biggan.py
@@ -1,4 +1,5 @@
 from functools import partial
+from typing import ClassVar
 
 import torch
 from torch import nn
@@ -55,23 +56,19 @@ class Block(nn.Module):
         self,
         in_channels: int,
         out_channels: int,
-        upsample: bool,
-        downsample: bool,
-        batch_norm: bool,
+        upsample: bool = True,
+        downsample: bool = False,
+        batch_norm: bool = True,
         kernel_size: int = 3,
         padding: int = 1,
         stride: int = 1,
     ) -> None:
         super().__init__()
-        self.conv1 = spectral_norm(
-            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
-        )
-        self.conv2 = spectral_norm(
-            nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
-        )
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
         self.skip_proj = False
         if in_channels != out_channels or upsample or downsample:
-            self.conv_sc = spectral_norm(nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0))
+            self.conv_sc = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
             self.skip_proj = True
 
         self.maybe_upsample = partial(F.interpolate, scale_factor=2) if upsample else nn.Identity()
@@ -100,6 +97,11 @@ class Block(nn.Module):
 
 
 class GeneratorBlock(Block):
+    """
+    Note:
+        Using spectral norm in generator & encoder didn't work well in preliminary experiments
+    """
+
     def __init__(
         self,
         in_channels: int,
@@ -123,7 +125,31 @@ class GeneratorBlock(Block):
         )
 
 
-class DiscriminatorBlock(Block):
+class EncoderBlock(Block):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        upsample: bool = False,
+        downsample: bool = True,
+        batch_norm: bool = True,
+        kernel_size: int = 3,
+        padding: int = 1,
+        stride: int = 1,
+    ) -> None:
+        super().__init__(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            upsample=upsample,
+            downsample=downsample,
+            batch_norm=batch_norm,
+            kernel_size=kernel_size,
+            padding=padding,
+            stride=stride,
+        )
+
+
+class SNDiscriminatorBlock(Block):
     def __init__(
         self,
         in_channels: int,
@@ -136,6 +162,9 @@ class DiscriminatorBlock(Block):
         stride: int = 1,
     ) -> None:
         super().__init__(in_channels, out_channels, upsample, downsample, batch_norm, kernel_size, padding, stride)
+        self.conv1 = spectral_norm(self.conv1)
+        self.conv2 = spectral_norm(self.conv2)
+        self.conv_sc = spectral_norm(self.conv_sc)
 
 
 class Generator128(nn.Module):
@@ -171,30 +200,32 @@ class Encoder128(nn.Module):
         This is based on the implementation of Discriminator.
     """
 
+    block: ClassVar[type[Block]] = EncoderBlock
+
     def __init__(self, latent_dim: int = 128, middle_dim: int = 96, in_channels: int = 3) -> None:
         super().__init__()
         self.pre_convs = nn.Sequential(
-            spectral_norm(nn.Conv2d(in_channels, middle_dim, kernel_size=3, padding=1)),
-            nn.ReLU(),
-            spectral_norm(nn.Conv2d(middle_dim, middle_dim, kernel_size=3, padding=1)),
+            nn.Conv2d(in_channels, middle_dim, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(middle_dim, middle_dim, kernel_size=3, padding=1),
             nn.AvgPool2d(kernel_size=2),
         )
-        self.pre_skip = spectral_norm(nn.Conv2d(in_channels, middle_dim, kernel_size=1))
+        self.pre_skip = nn.Conv2d(in_channels, middle_dim, kernel_size=1)
         self.pool = nn.AvgPool2d(kernel_size=2)
 
         self.convs = nn.Sequential(
-            DiscriminatorBlock(middle_dim, middle_dim),  # 64 -> 32
-            DiscriminatorBlock(middle_dim, 2 * middle_dim),  # 32 -> 16
+            self.block(middle_dim, middle_dim),  # 64 -> 32
+            self.block(middle_dim, 2 * middle_dim),  # 32 -> 16
             SelfAttention(2 * middle_dim),
-            DiscriminatorBlock(middle_dim * 2, middle_dim * 4),  # 16 -> 8
-            DiscriminatorBlock(middle_dim * 4, middle_dim * 8),  # 8 -> 4
-            DiscriminatorBlock(middle_dim * 8, middle_dim * 16),  # 4 -> 2
-            DiscriminatorBlock(middle_dim * 16, middle_dim * 16, downsample=False),  # 2 -> 2
+            self.block(middle_dim * 2, middle_dim * 4),  # 16 -> 8
+            self.block(middle_dim * 4, middle_dim * 8),  # 8 -> 4
+            self.block(middle_dim * 8, middle_dim * 16),  # 4 -> 2
+            self.block(middle_dim * 16, middle_dim * 16, downsample=False),  # 2 -> 2
             nn.ReLU(inplace=True),
         )
         self.relu = nn.ReLU(inplace=True)
 
-        self.final_projection = spectral_norm(nn.Linear(middle_dim * 16, latent_dim))
+        self.final_projection = nn.Linear(middle_dim * 16, latent_dim)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         skip = self.pre_skip(self.pool(x))
@@ -207,5 +238,12 @@ class Encoder128(nn.Module):
 
 
 class Discriminator128(Encoder128):
+    block = SNDiscriminatorBlock
+
     def __init__(self, middle_dim: int = 96, in_channels: int = 3) -> None:
         super().__init__(latent_dim=1, middle_dim=middle_dim, in_channels=in_channels)
+        for i, module in enumerate(self.pre_convs.modules()):
+            if isinstance(module, nn.Conv2d):
+                self.pre_convs[i] = spectral_norm(module)
+        self.pre_skip = spectral_norm(self.pre_skip)
+        self.final_projection = spectral_norm(self.final_projection)
