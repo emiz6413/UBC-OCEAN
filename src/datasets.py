475680de36290a3bd4ce4384f95b81ac533799b0
emiz6413 <mizutanieisuke+github@gmail.com>
2023-11-16 11:17:39 +0800

    update constructor

--- src/datasets.py
+++ src/datasets.py
@@ -1,13 +1,15 @@
+from itertools import chain
 from pathlib import Path
 
 import torch
 from torch.utils.data import Dataset
 from torchvision import io, transforms
+from tqdm.auto import tqdm
 
 
 class UBCODataset(Dataset):
-    def __init__(self, image_paths: list[Path], transforms: transforms.Compose) -> None:
-        self.image_paths = image_paths
+    def __init__(self, image_dirs: list[Path], transforms: transforms.Compose) -> None:
+        self.image_paths = list(chain.from_iterable([d.glob("*.png") for d in tqdm(image_dirs)]))
         self.transforms = transforms
 
     def __len__(self) -> int:
