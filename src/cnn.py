0e86a593f82a41bc51747cc9cc86ebe9543c19db
emiz6413 <mizutanieisuke+github@gmail.com>
2023-12-12 22:13:08 +0800

    add training code

--- src/cnn.py
+++ src/cnn.py
@@ -1,8 +1,13 @@
 import math
 
+import numpy as np
 import torch
-from torch import Tensor, nn
+from torch import Tensor, nn, optim
+from torch.utils.data import DataLoader
 from torchvision.models.convnext import CNBlock  # type: ignore[import-untyped]
+from tqdm.auto import tqdm
+
+from .utils import AverageMeter
 
 
 class SpatialPyramidPooling(nn.Module):
@@ -34,7 +39,13 @@ class SpatialPyramidPooling(nn.Module):
 
 
 class CNN(nn.Module):
-    def __init__(self, in_channels: int, num_classes: int, num_pools: tuple[int, ...] = (1, 4, 16)) -> None:
+    def __init__(
+        self,
+        in_channels: int,
+        num_classes: int,
+        num_pools: tuple[int, ...] = (1, 4, 16),
+        device: torch.device = torch.device("cuda"),
+    ) -> None:
         super().__init__()
         self.stages = nn.Sequential(
             CNBlock(dim=in_channels, layer_scale=1e-6, stochastic_depth_prob=0.1),
@@ -46,10 +57,64 @@ class CNN(nn.Module):
         self.classifier = nn.Sequential(
             nn.LayerNorm(in_channels, eps=1e-6), nn.Flatten(start_dim=1), nn.Linear(in_channels, num_classes)
         )
+        self.optimizer = self.configure_optimizer()
+        self.criterion = self.configure_criterion()
+        if device is None:
+            device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        self.device = device
+
+    def configure_optimizer(self) -> optim.Optimizer:
+        self.optimizer = optim.Adam(self.parameters(), lr=1e-4)
+        return self.optimizer
+
+    def configure_criterion(self) -> nn.Module:
+        self.criterion = nn.CrossEntropyLoss(reduction="mean")
+        return self.criterion
 
     def forward(self, x: Tensor) -> Tensor:
+        x = x.to(device=self.device)
         x = self.stages(x)
         x = self.spatial_pyramid_pool(x)
         x = x.flatten(start_dim=1)  # (bs, ch, sum(pools)) -> (bs, ch * sum(pools))
         x = self.classifier(x)
         return x
+
+    def train_step(self, x: Tensor, label: Tensor) -> tuple[Tensor, Tensor]:
+        pred = self.forward(x)
+        loss = self.criterion(input=pred, target=label.to(pred.device))
+        loss.backward()
+        return loss, pred
+
+    @torch.no_grad()
+    def eval_step(self, x: Tensor, label: Tensor) -> tuple[Tensor, Tensor]:
+        pred = self.forward(x)
+        loss = self.criterion(input=pred, target=label.to(pred.device))
+        return loss, pred
+
+    def train_single_epoch(self, data_loader: DataLoader) -> tuple[float, np.ndarray, np.ndarray]:
+        preds = []
+        labels = []
+        loss = AverageMeter()
+        pbar = tqdm(data_loader)
+        for x, label in pbar:
+            _loss, pred = self.train_step(x, label)
+            loss.update(_loss.item())
+            preds.append(pred.detach().cpu().numpy())
+            labels.append(label.numpy())
+            pbar.set_description(f"loss: {loss.average:.3f}")
+
+        return loss.average, np.concatenate(preds, axis=0), np.concatenate(labels, axis=0)
+
+    def evaluate(self, data_loader: DataLoader) -> tuple[float, np.ndarray, np.ndarray]:
+        preds = []
+        labels = []
+        loss = AverageMeter()
+        pbar = tqdm(data_loader)
+        for x, label in pbar:
+            _loss, pred = self.eval_step(x, label)
+            loss.update(_loss.item())
+            preds.append(pred.cpu().numpy())
+            labels.append(label.numpy())
+            pbar.set_description(f"loss: {loss.average:.3f}")
+
+        return loss.average, np.concatenate(preds, axis=0), np.concatenate(labels, axis=0)
