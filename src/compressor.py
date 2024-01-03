e630d63ac4faf6148169307e5d488181b1efbf7d
emiz6413 <mizutanieisuke+github@gmail.com>
2024-01-03 22:50:34 +0900

    Dev to main (#5)
    * Revert "use spectral norm for generator & encoder"
    
    This reverts commit c90d45c79492621085972f30f1be79eb14928c8b.
    
    * add BigGAN
    
    * iterate D twice
    
    * apply spectral norm only to discriminator
    
    * move torchvision & scipy to main
    
    * fix BigGAN modules
    
    * add matplotlib
    
    * bug fix
    
    * fix compatibility
    
    * update README
    
    * fix data loading
    
    * fix autocast switch
    
    * delete debug code
    
    * let autocast handle dtype
    
    * refactor
    
    * add sn_enabled argument
    
    * reproduce biggan code
    
    * change lr to the numbers in paper
    
    * adjust architecture as described in the paper
    
    * add auto-encoder
    
    * add UBCO dataset
    
    * change default lr
    
    * update constructor
    
    * add Compressor
    
    * ignore warning
    
    * add VAR
    
    * fix bug
    
    * add amp
    
    * raise error on nan or inf
    
    * add BarlowTwins
    
    * fix transform
    
    * fix transform
    
    * update transform scale
    
    * update hyper-parameters
    
    * update readme
    
    * fix syntax error
    
    * fix format
    
    * update readme
    
    * update readme
    
    * fix VAE eps
    
    * add bidirectional gan
    
    * add Enc/Gen/Disc64
    
    * add bn to disc and weight init
    
    * make bn in discriminator optional
    
    * change the default value
    
    * critical bug fix
    
    * unify spectral_norm
    
    * update README
    
    * add TypeVar
    
    * delete old modules
    
    * add 128 modules
    
    * update architecture
    
    * fix PatchedImage
    
    * update docstring
    
    * reduce memory in compressor
    
    * delete dataset
    
    * add CNN with
    SpatialPyramidPooling
    
    * delete vaegan
    
    * add training code
    
    * add amp training
    
    * update compress
    
    * add pyvips & pywuffs
    
    * update compressor
    
    * delete old code
    
    * update README
    
    * fix typo
    
    ---------
    
    Co-authored-by: emiz6413 <mizutanieisuke+github.com>

--- src/compressor.py
+++ src/compressor.py
@@ -1,25 +1,29 @@
 import math
 
-import numpy as np
 import torch
 from torch import Tensor, nn
+from torch.cuda.amp.autocast_mode import autocast
 from torch.nn import functional as F
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms  # type: ignore[import-untyped]
 from tqdm.auto import tqdm
 
 
-class PatchedImage(Dataset):
+class PatchedImageWuffs(Dataset):
+    """Much faster but load the entire image in memory"""
+
     def __init__(
-        self, img: Tensor, transforms: transforms.Compose, patch_size: int = 128, pad_value: float = 0.0
+        self,
+        img: Tensor,
+        patch_size: int = 128,
+        pad_value: int = 0,
+        dtype: torch.dtype = torch.float,
     ) -> None:
-        self.img = img
-        _, h, w = self.img.shape
-        self.n_x = math.ceil(w / patch_size)
-        self.n_y = math.ceil(h / patch_size)
-        self.patch_size = patch_size
-        self.pad_value = pad_value
-        self.transforms = transforms
+        img = self.pad(img, patch_size, pad_value)
+        self.img = self.tile(img, patch_size)
+        self.n_y = self.img.size(1)
+        self.n_x = self.img.size(2)
+        self.dtype = dtype
 
     def __len__(self) -> int:
         return self.n_x * self.n_y
@@ -27,37 +31,46 @@ class PatchedImage(Dataset):
     def __getitem__(self, index: int) -> Tensor:
         idx_y = index // self.n_x
         idx_x = index % self.n_x
-        x_start = idx_x * self.patch_size
-        y_start = idx_y * self.patch_size
-        patch = self.img[:, y_start : y_start + self.patch_size, x_start : x_start + self.patch_size]
-        patch = self.maybe_pad(patch)
-        patch = self.transforms(patch)
-        return patch
+        return self.img[:, idx_y, idx_x]
 
-    def maybe_pad(self, patch: Tensor) -> Tensor:
-        if (patch.size(1), patch.size(2)) == (self.patch_size, self.patch_size):
-            return patch
-        pad = (0, self.patch_size - patch.size(2), 0, self.patch_size - patch.size(1), 0, 0)
-        return F.pad(patch, pad, value=self.pad_value)
+    @staticmethod
+    def tile(img: Tensor, patch_size: int) -> Tensor:
+        return img.unfold(1, patch_size, patch_size).unfold(2, patch_size, patch_size)  # 3,squeezed_h,squeezed_w,p,p
+
+    @staticmethod
+    def pad(img: Tensor, patch_size: int, pad_value: int) -> Tensor:
+        """make height and width multiple of patch size"""
+        _, h, w = img.shape
+        target_h = int(math.ceil(float(h) / patch_size) * patch_size)
+        target_w = int(math.ceil(float(w) / patch_size) * patch_size)
+        pad = (0, target_w - w, 0, target_h - h, 0, 0)
+        return F.pad(img, pad, value=pad_value)
 
 
-class Comporessor:
+class Compressor:
     def __init__(
         self,
         encoder: nn.Module,
+        transforms: transforms.Compose,
         batch_size: int,
         device: torch.device | None = None,
+        num_workers: int = 0,
+        amp: bool = True,
     ) -> None:
         self.encoder = encoder.eval()
+        self.transforms = transforms
         self.batch_size = batch_size
+        self.num_workers = num_workers
         if device is None:
             device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
         self.device = device
+        self.amp = amp
 
     @torch.no_grad()
-    def compress(self, img: PatchedImage) -> np.ndarray:
-        loader = DataLoader(img, batch_size=self.batch_size, shuffle=False)
-        features = np.concatenate([self.encoder(x.to(self.device)).cpu().numpy() for x in tqdm(loader)], axis=0)
+    def compress(self, img: PatchedImageWuffs) -> Tensor:
+        loader = DataLoader(img, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=False)
+        with autocast(enabled=self.amp):
+            features = torch.concat([self.encoder(self.transforms(x.to(self.device))) for x in tqdm(loader)], dim=0)
         features = features.squeeze()
-        features = features.reshape(img.n_y, img.n_x, features.shape[-1])  # (xy, ch) -> (y, x, ch)
-        return features.transpose(2, 0, 1)  # (y, x, ch) -> (ch, y, x)
+        features = features.view(img.n_y, img.n_x, features.shape[-1])  # (xy, ch) -> (y, x, ch)
+        return features.permute(2, 0, 1)  # (y, x, ch) -> (ch, y, x)
